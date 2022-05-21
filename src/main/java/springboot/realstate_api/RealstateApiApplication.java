e646bf61e0b6922cce7a2ad2b910a9c112f12cc9
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2022-05-20 22:01:32 -0500

    Initial project with dependencies

---
+++ src/main/java/springboot/realstate_api/RealstateApiApplication.java
@@ -0,0 +1,17 @@
+package springboot.realstate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealstateApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RealstateApiApplication.class, args);
+
+
+	}
+
+
+}
---
+++ src/test/java/springboot/realstate_api/RealstateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.realstate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealstateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
